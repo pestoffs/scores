34222ca0f717a3949f8538852905aa9b50830eec
venyarazrabotchik <venavena5879@gmail.com>
2024-06-26 16:57:21 +0300

    added func writerecord

---
+++ lib.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func WriteRecord() {
+	file, err := os.OpenFile("test.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	defer file.Close()
+
+	//fmt.Printf("%s;%d\n", "user", 546473)
+	_, err = fmt.Fprintf(file, "%s;%d\n", "user", 546473)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+
+}
