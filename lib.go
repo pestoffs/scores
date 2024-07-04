883f3333bdd1a73155a579c60849d8b658effc97
venyarazrabotchik <venavena5879@gmail.com>
2024-07-04 15:40:47 +0300

    venya new func

--- lib.go
+++ lib.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func WriteRecord(user string, score int) {
@@ -35,4 +36,27 @@ func ReadRecord(filename string) error {
 		fmt.Println(scanner.Text())
 	}
 	return scanner.Err()
+}
+
+func findUserScore(filename, user string) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ";")
+		if len(parts) == 2 && parts[0] == user {
+			var score int
+			fmt.Sscanf(parts[1], "%d", &score)
+			return score, nil
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+	return 0, fmt.Errorf("user not found")
 }
